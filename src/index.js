e823f16624cf29ddd30cdf5e4d1742257e09af5a
Diego Fernandes <diego.schell.f@gmail.com>
2017-08-23 20:35:38 -0300

    Render same root component to both platforms

---
+++ src/index.js
@@ -0,0 +1,20 @@
+/* @flow */
+
+import React, { Component } from 'react';
+
+import {
+  View,
+  Text,
+} from 'react-native';
+
+class RocketCast extends Component {
+  render() {
+    return (
+      <View>
+        <Text>Hey</Text>
+      </View>
+    );
+  }
+}
+
+export default RocketCast;
