3d7102f2e60180db3d4a98cee0f57dbb04a047ea
Diego Fernandes <diego.schell.f@gmail.com>
2017-08-23 22:20:46 -0300

    ESLint configuration with AirBnB Style Guides

--- src/index.js
+++ src/index.js
@@ -8,6 +8,12 @@ import {
 } from 'react-native';
 
 class RocketCast extends Component {
+  constructor(props) {
+    super(props);
+
+    this.state = {};
+  }
+
   render() {
     return (
       <View>
---
+++ src/lib/Api.js
@@ -0,0 +1,9 @@
+/* @flow */
+
+import { create } from 'apisauce'
+
+const api = create({
+  baseURL: 'http://localhost:3000/',
+});
+
+export default api;
